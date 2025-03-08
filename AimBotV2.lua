9061ff6c79a87d655855050751642ede21137abf
FlamesScript <realflamesclips+Github@gmail.com>
2025-03-07 21:51:17 -0500

    Update AimBotV2.lua

--- AimBotV2.lua
+++ AimBotV2.lua
@@ -12,7 +12,7 @@ local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local camera = workspace.CurrentCamera
 
-gengenv().SETTINGS = {
+getgenv().SETTINGS = {
     AimLockOn = false,
     TargetPart = "Head",
     FovCircleSize = 70,
